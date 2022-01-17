d7496d2aee64de4b288ba861c61bc606df9a47b9
Raúl Mortes <correomortes@gmail.com>
2022-01-17 19:02:28 +0100

    Fixed #3

--- src/main/java/place/imphi/mods/focus/gui/description/ShulkerLugGuiDescription.kt
+++ src/main/java/place/imphi/mods/focus/gui/description/ShulkerLugGuiDescription.kt
@@ -10,22 +10,23 @@ import net.minecraft.screen.ScreenHandlerContext
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.screen.slot.Slot
 import place.imphi.mods.focus.block_entities.ShulkerLugBlockEntity
+import place.imphi.mods.focus.blocks.ShulkerLugBlock
 
 /**
  * Extremely simple GUI description, provided with <3 by cottonmc
  * It's a glorified dispenser interface, ngl
  */
 class ShulkerLugGuiDescription(syncId: Int, playerInventory: PlayerInventory, context: ScreenHandlerContext) :
-    SyncedGuiDescription(ScreenHandlerType.GENERIC_3X3, syncId, playerInventory, getBlockInventory(context, ShulkerLugBlockEntity.INVENTORY_SIZE), getBlockPropertyDelegate(context)) {
+    SyncedGuiDescription(ShulkerLugBlock.SHULKER_LUG_SCREEN_HANDLER_TYPE, syncId, playerInventory, getBlockInventory(context, ShulkerLugBlockEntity.INVENTORY_SIZE), getBlockPropertyDelegate(context)) {
     init {
         val root = WGridPanel()
         setRootPanel(root)
-        root.setSize(300, 200)
+//        root.setSize(200, 200)
         root.insets = Insets.ROOT_PANEL
         val itemSlot = WItemSlot.of(blockInventory, 0, 3, 3)
         itemSlot.setFilter { toInsert -> toInsert.item.canBeNested() }
-        root.add(itemSlot, 4, 1)
-        root.add(createPlayerInventoryPanel(), 0, 3)
+        root.add(itemSlot, 3, 1)
+        root.add(createPlayerInventoryPanel(), 0, 4)
         root.validate(this)
     }
 }
